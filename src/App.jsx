dbd13a219f7434f73347ac298723e7fee8c075f3
Manoj <manoj@elbrit.org>
2025-12-24 15:35:42 +0530

    initial react component project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Buttons from "./components/Buttons";
+
+function App() {
+  return <Buttons />;
+}
+
+export default App;
---
+++ src/components/Buttons.jsx
@@ -0,0 +1,11 @@
+import "./Buttons.css";
+
+export default function Buttons() {
+  return (
+    <div className="btn-container">
+      <button className="btn primary">Primary</button>
+      <button className="btn secondary">Secondary</button>
+      <button className="btn danger">Danger</button>
+    </div>
+  );
+}
